cbf95de3611bc602df478c4ebc6925b38b0a4be3
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-10-04 11:06:20 +0200

    Gateway Service Initialization

---
+++ src/test/java/com/dgmf/gateway/service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.gateway.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
